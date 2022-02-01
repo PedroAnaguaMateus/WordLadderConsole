5cf7affea3bd5d81f8321c35d1eaad748143424a
PedroAnaguaMateus <pedro.mateus@fccn.pt>
2022-02-01 16:43:57 +0000

    Environment removed.

--- WordLadderConsole/Program.cs
+++ WordLadderConsole/Program.cs
@@ -13,7 +13,7 @@ Filehandler filehandler = new();
 List<string> allwords = new();
 string firstWord, lastWord, resultsfile;
 
-await filehandler.GetAllWordsFromFile(allwords, Environment.CurrentDirectory + "\\" + GetConsoleLineAnswer(Constants.DictonaryFileRequest));
+await filehandler.GetAllWordsFromFile(allwords, GetConsoleLineAnswer(Constants.DictonaryFileRequest));
 
 firstWord = GetConsoleLineAnswer(Constants.StartWordRequest);
 lastWord = GetConsoleLineAnswer(Constants.LastWordRequest);
@@ -23,6 +23,6 @@ consoleControler = new ConsoleController(firstWord, lastWord, allwords);
 var results = consoleControler.GetLadders();
 
 if (results.Count > 0)
-    await filehandler.WriteLadderToFile(Environment.CurrentDirectory + "\\" + resultsfile, results[0].ToList<string>());
+    await filehandler.WriteLadderToFile(resultsfile, results[0].ToList<string>());
 
 #endregion
